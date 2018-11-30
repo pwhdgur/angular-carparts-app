dc9ec434a9a54aea2aa22d387cafabc4de31a264
mulzomdao <mulzomdao@gmail.com>
2018-11-30 21:55:15 +0900

    v 2.0

--- src/app/carparts/carparts-data.service.ts
+++ src/app/carparts/carparts-data.service.ts
@@ -1,7 +1,11 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {CarPart} from './car-part';
+
+const httpOptions = {
+ headers: new HttpHeaders ({'Content-Type': 'application/json'});
+}
 
 @Injectable( /*{
   providedIn : 'root'
@@ -22,6 +26,11 @@ export class CarpartsDataService {
                           .toPromise()
                           .then(res => res['data'])
                           .catch(err => console.log('Error 발생',err));
+  }
+
+  addCarPart(carPart: CarPart): Observable<CarPart> {
+    carPart.id = Number(carPart.id);
+    return this.httpClient.post<CarPart>('http://127.0.0.1:52274/products', CarPart, httpOptions);
   }
 
 }
